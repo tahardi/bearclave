73efa73c6267288831faab516127f1f693c69f44
Taylor Antonio Hardin <Taylor.Antonio.Hardin@gmail.com>
2025-04-27 15:54:23 -0400

    Reduce context timeout

--- examples/hello-world/enclave/main.go
+++ examples/hello-world/enclave/main.go
@@ -98,7 +98,7 @@ func main() {
 		panic(err)
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	fmt.Printf("Listening on: %s\n", enclaveAddr)
--- examples/hello-world/nonclave/main.go
+++ examples/hello-world/nonclave/main.go
@@ -99,7 +99,7 @@ func main() {
 		panic(err)
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	fmt.Printf("Sending to: %s\n", enclaveAddr)
