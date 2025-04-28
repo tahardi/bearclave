ddb36123b902e7a83e6b284a7f9453566eab4025
Taylor Hardin <Taylor.Antonio.Hardin@gmail.com>
2025-04-28 12:34:32 -0400

    Nitro Implementation (Update) (#3)
    * Inital Nitro implementation
    
    * Reorg type interface and funcs
    
    * Update to work with Nitro implementation
    
    * Remove New func that was meant for testing
    
    * Add notes about hf
    
    * Tidy
    
    * Add build targets for executables and enclave docker image
    
    * Use multistage build and scratch image to minimize enclave image size
    
    * Use background context without timeout so enclave will wait indefinitely for a connection
    
    * Add targets for building and running eifs
    
    * Ignore all bin folders in project
    
    * Add steps for setting up EC2 instance for Nitro development
    
    * Remove the notion of a platform detector. Instead, KISS and assume that gets passed into the program via flags or a config
    
    * Remove the notion of a platform detector. Instead, KISS and assume that gets passed into the program via flags or a config
    
    * Call make clean in examples directories too

--- examples/hello-world/enclave/main.go
+++ examples/hello-world/enclave/main.go
@@ -4,73 +4,46 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"time"
 
 	"github.com/tahardi/bearclave"
+	"github.com/tahardi/bearclave/examples/hello-world/sdk"
 )
 
-func MakeDetectors() ([]bearclave.Detector, error) {
-	cvmsDetector, err := bearclave.NewCVMSDetector()
-	if err != nil {
-		return nil, fmt.Errorf("creating CVMS detector: %w", err)
+func MakeAttester(platform sdk.Platform) (bearclave.Attester, error) {
+	switch platform {
+	case sdk.ConfidentialVMs:
+		return bearclave.NewCVMSAttester()
+	case sdk.Nitro:
+		return bearclave.NewNitroAttester()
+	case sdk.Unsafe:
+		return bearclave.NewUnsafeAttester()
+	default:
+		return nil, fmt.Errorf("unsupported platform '%s'", platform)
 	}
-	nitroDetector, err := bearclave.NewNitroDetector()
-	if err != nil {
-		return nil, fmt.Errorf("creating Nitro detector: %w", err)
-	}
-	unsafeDetector, err := bearclave.NewUnsafeDetector()
-	if err != nil {
-		return nil, fmt.Errorf("creating Unsafe detector: %w", err)
-	}
-	return []bearclave.Detector{
-		cvmsDetector,
-		nitroDetector,
-		unsafeDetector,
-	}, nil
 }
 
-func MakeAttester(detectors []bearclave.Detector) (bearclave.Attester, error) {
-	for _, detector := range detectors {
-		platform, ok := detector.Detect()
-		switch {
-		case !ok:
-			continue
-		case platform == bearclave.CVMSPlatform:
-			return bearclave.NewCVMSAttester()
-		case platform == bearclave.NitroPlatform:
-			return bearclave.NewNitroAttester()
-		case platform == bearclave.UnsafePlatform:
-			return bearclave.NewUnsafeAttester()
-		}
+func MakeCommunicator(platform sdk.Platform) (bearclave.Communicator, error) {
+	switch platform {
+	case sdk.ConfidentialVMs:
+		return bearclave.NewCVMSCommunicator()
+	case sdk.Nitro:
+		return bearclave.NewNitroCommunicator(
+			nitroNonclaveCID,
+			nitroNonclavePort,
+			nitroEnclavePort,
+		)
+	case sdk.Unsafe:
+		return bearclave.NewUnsafeCommunicator(
+			unsafeNonclaveAddr,
+			unsafeEnclaveAddr,
+		)
+	default:
+		return nil, fmt.Errorf("unsupported platform '%s'", platform)
 	}
-	return nil, fmt.Errorf("no supported platforms detected")
 }
 
-func MakeCommunicator(detectors []bearclave.Detector) (bearclave.Communicator, error) {
-	for _, detector := range detectors {
-		platform, ok := detector.Detect()
-		switch {
-		case !ok:
-			continue
-		case platform == bearclave.CVMSPlatform:
-			return bearclave.NewCVMSCommunicator()
-		case platform == bearclave.NitroPlatform:
-			return bearclave.NewNitroCommunicator(
-				nitroNonclaveCID,
-				nitroNonclavePort,
-				nitroEnclavePort,
-			)
-		case platform == bearclave.UnsafePlatform:
-			return bearclave.NewUnsafeCommunicator(
-				unsafeNonclaveAddr,
-				unsafeEnclaveAddr,
-			)
-		}
-	}
-	return nil, fmt.Errorf("no supported platforms detected")
-}
+var platform string
 
-var nitroEnclaveCID int
 var nitroNonclaveCID int
 var nitroEnclavePort int
 var nitroNonclavePort int
@@ -79,12 +52,14 @@ var unsafeEnclaveAddr string
 var unsafeNonclaveAddr string
 
 func main() {
-	flag.IntVar(
-		&nitroEnclaveCID,
-		"nitroEnclaveCID",
-		bearclave.NitroEnclaveCID,
-		"The context ID that the enclave should use when using Nitro",
+	flag.StringVar(
+		&platform,
+		"platform",
+		"unsafe",
+		"The Trusted Computing platform to use. Options: "+
+			"cvms, nitro, unsafe (default: unsafe)",
 	)
+
 	flag.IntVar(
 		&nitroNonclaveCID,
 		"nitroNonclaveCID",
@@ -117,24 +92,17 @@ func main() {
 		"The address that the non-enclave should listen on when using Unsafe",
 	)
 
-	detectors, err := MakeDetectors()
+	attester, err := MakeAttester(sdk.Platform(platform))
 	if err != nil {
 		panic(err)
 	}
 
-	attester, err := MakeAttester(detectors)
+	communicator, err := MakeCommunicator(sdk.Platform(platform))
 	if err != nil {
 		panic(err)
 	}
 
-	communicator, err := MakeCommunicator(detectors)
-	if err != nil {
-		panic(err)
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-
+	ctx := context.Background()
 	fmt.Printf("Listening on: %s\n", unsafeEnclaveAddr)
 	userdata, err := communicator.Receive(ctx)
 	if err != nil {
---
+++ examples/hello-world/sdk/platform.go
@@ -0,0 +1,9 @@
+package sdk
+
+type Platform string
+
+const (
+	ConfidentialVMs Platform = "cvms"
+	Nitro           Platform = "nitro"
+	Unsafe          Platform = "unsafe"
+)
