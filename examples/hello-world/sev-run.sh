51cb84933a0f513bf3922812388fec6826dafc46
Taylor Hardin <taylor.a.hardin.gr@dartmouth.edu>
2025-05-09 09:41:12 -1000

    Try using awk instead of sed bc awk wont buffer output

--- examples/hello-world/sev-run.sh
+++ examples/hello-world/sev-run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Run our enclave and proxy in the background and capture both their STDOUT and STDERR output.
 # Prepend each line of output with the program name for better readability as both the
-# enclave and proxy output will intermingle in the terminal. Tell sed not to buffer output.
-/app/enclave --config /app/enclave-config.yaml 2>&1 | sed -u 's/^/[enclave] /' &
+# enclave and proxy output will intermingle in the terminal.
+/app/enclave --config /app/enclave-config.yaml 2>&1 | awk '{ print "[enclave] " $0; fflush(); }' &
 ENCLAVE_PID=$!
 
-/app/enclave-proxy --config /app/proxy-config.yaml 2>&1 | sed -u 's/^/[enclave-proxy] /' &
+/app/enclave-proxy --config /app/proxy-config.yaml 2>&1 | awk '{ print "[enclave-proxy] " $0; fflush(); }' &
 PROXY_PID=$!
 
 # Wait for either process to exit
