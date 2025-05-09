f456a1c265118d334e346be1b369f66d3ee46cb6
Taylor Hardin <taylor.a.hardin.gr@dartmouth.edu>
2025-05-08 16:57:51 -1000

    Add in length check

--- internal/sev/attester.go
+++ internal/sev/attester.go
@@ -9,5 +9,32 @@ func NewAttester() (*Attester, error) {
 }
 
 func (n *Attester) Attest(userdata []byte) ([]byte, error) {
+	if len(userdata) > AMD_SEV_USERDATA_SIZE {
+		return []byte("user data too long"), nil
+		//return nil, fmt.Errorf(
+		//	"userdata must be less than %d bytes",
+		//	AMD_SEV_USERDATA_SIZE,
+		//)
+	}
+	//attestation := []byte("in sev-snp attester: ")
+	//attestation = append(attestation, userdata...)
+	//return attestation, nil
+
+	//sevQP, err := client.GetQuoteProvider()
+	//if err != nil {
+	//	return nil, fmt.Errorf("getting quote provider: %w", err)
+	//}
+	//
+	//if !sevQP.IsSupported() {
+	//	return nil, fmt.Errorf("SEV is not supported")
+	//}
+	//
+	//var reportData [64]byte
+	//copy(reportData[:], userdata)
+	//attestation, err := sevQP.GetRawQuote(reportData)
+	//if err != nil {
+	//	return nil, fmt.Errorf("getting quote: %w", err)
+	//}
+	//return attestation, nil
 	return userdata, nil
 }
