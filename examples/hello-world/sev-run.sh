accccea2baf3c47486f64265513496a67e60996b
Taylor Hardin <taylor.a.hardin.gr@dartmouth.edu>
2025-05-09 09:26:35 -1000

    Tell sed to output each line as they come instead of buffering

--- examples/hello-world/sev-run.sh
+++ examples/hello-world/sev-run.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-/app/enclave --config /app/enclave-config.yaml 2>&1 | sed 's/^/[enclave] /' &
+# Run our enclave and proxy in the background and capture both their STDOUT and STDERR output.
+# Prepend each line of output with the program name for better readability as both the
+# enclave and proxy output will intermingle in the terminal. Tell sed not to buffer output.
+/app/enclave --config /app/enclave-config.yaml 2>&1 | sed -u 's/^/[enclave] /' &
 ENCLAVE_PID=$!
 
-/app/enclave-proxy --config /app/proxy-config.yaml 2>&1 | sed 's/^/[enclave-proxy] /' &
+/app/enclave-proxy --config /app/proxy-config.yaml 2>&1 | sed -u 's/^/[enclave-proxy] /' &
 PROXY_PID=$!
 
-# Handle process termination
-trap 'kill $ENCLAVE_PID $PROXY_PID; exit' TERM INT
-
 # Wait for either process to exit
+trap 'kill $ENCLAVE_PID $PROXY_PID; exit' TERM INT
 wait -n $ENCLAVE_PID $PROXY_PID
-
-# If we get here, one of the processes died, so exit with error
 exit 1
