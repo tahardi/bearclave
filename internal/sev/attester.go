5c847f3e0e8e8b50376fc0ecfdca8cc4428d0ed2
Taylor Hardin <taylor.a.hardin.gr@dartmouth.edu>
2025-05-08 17:04:43 -1000

    Try getting quote provider and seeing if sev is supported

--- internal/sev/attester.go
+++ internal/sev/attester.go
@@ -1,5 +1,10 @@
 package sev
 
+import (
+	"fmt"
+	"github.com/google/go-sev-guest/client"
+)
+
 const AMD_SEV_USERDATA_SIZE = 64
 
 type Attester struct{}
@@ -10,25 +15,27 @@ func NewAttester() (*Attester, error) {
 
 func (n *Attester) Attest(userdata []byte) ([]byte, error) {
 	if len(userdata) > AMD_SEV_USERDATA_SIZE {
-		return []byte("user data too long"), nil
+		msg := fmt.Sprintf("userdata must be less than %d bytes", AMD_SEV_USERDATA_SIZE)
+		return []byte(msg), nil
 		//return nil, fmt.Errorf(
 		//	"userdata must be less than %d bytes",
 		//	AMD_SEV_USERDATA_SIZE,
 		//)
 	}
-	//attestation := []byte("in sev-snp attester: ")
-	//attestation = append(attestation, userdata...)
-	//return attestation, nil
 
-	//sevQP, err := client.GetQuoteProvider()
-	//if err != nil {
-	//	return nil, fmt.Errorf("getting quote provider: %w", err)
-	//}
-	//
-	//if !sevQP.IsSupported() {
-	//	return nil, fmt.Errorf("SEV is not supported")
-	//}
-	//
+	sevQP, err := client.GetQuoteProvider()
+	if err != nil {
+		msg := fmt.Sprintf("error getting quote provider: %s", err)
+		return []byte(msg), nil
+		//return nil, fmt.Errorf("getting quote provider: %w", err)
+	}
+
+	if !sevQP.IsSupported() {
+		msg := fmt.Sprintf("SEV is not supported")
+		return []byte(msg), nil
+		//return nil, fmt.Errorf("SEV is not supported")
+	}
+
 	//var reportData [64]byte
 	//copy(reportData[:], userdata)
 	//attestation, err := sevQP.GetRawQuote(reportData)
