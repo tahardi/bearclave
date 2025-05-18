75e6e1790e352d4e8620bab86fb03c654ee6ed06
Taylor Antonio Hardin <Taylor.Antonio.Hardin@gmail.com>
2025-05-18 17:52:31 -0400

    Lint

--- internal/networking/handlers_test.go
+++ internal/networking/handlers_test.go
@@ -10,7 +10,6 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-
 	"github.com/tahardi/bearclave/internal/networking"
 	"github.com/tahardi/bearclave/mocks"
 )
