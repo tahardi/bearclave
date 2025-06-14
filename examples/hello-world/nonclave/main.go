c9965895f2f5e356cb6573060681be41f1a0d3dd
Taylor Antonio Hardin <Taylor.Antonio.Hardin@gmail.com>
2025-06-14 18:16:25 -0400

    Remove default config string from internal and pkg since that is an example level decision

--- examples/hello-world/nonclave/main.go
+++ examples/hello-world/nonclave/main.go
@@ -19,7 +19,7 @@ func main() {
 	flag.StringVar(
 		&configFile,
 		"config",
-		setup.DefaultConfigFile,
+		"configs/nonclave/notee.yaml",
 		"The Trusted Computing platform to use. Options: "+
 			"nitro, sev, tdx, notee (default: notee)",
 	)
