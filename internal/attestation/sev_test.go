15fc94660d6d13c33b3645d05d3ad47e37b8772c
Taylor Antonio Hardin <Taylor.Antonio.Hardin@gmail.com>
2025-06-14 17:23:02 -0400

    Switch from deep equal to individually testing each field and returning a field-specific error

--- internal/attestation/sev_test.go
+++ internal/attestation/sev_test.go
@@ -290,20 +290,204 @@ func TestSEVVerifyMeasurement(t *testing.T) {
 		assert.ErrorContains(t, err, "unmarshaling measurement")
 	})
 
-	t.Run("error - incorrect measurement", func(t *testing.T) {
-		// given
-		measurement := sevReportMeasurementJSON
-		timestamp := time.Unix(
-			sevReportTimestampSeconds,
-			sevReportTimestampNanoseconds,
-		)
-		_, sevReport := sevReportFromTestData(t, sevReportB64, timestamp)
-		sevReport.Measurement[0] = 0
+	mismatchTestCases := []struct {
+		name         string
+		modifyReport func(*sevsnp.Report)
+		wantErr      string
+	}{
+		{
+			name: "error - version mismatch",
+			modifyReport: func(report *sevsnp.Report) {
+				report.Version += 1
+			},
+			wantErr: "version mismatch",
+		},
+		{
+			name: "error - guest svn mismatch",
+			modifyReport: func(report *sevsnp.Report) {
+				report.GuestSvn += 1
+			},
+			wantErr: "guest svn mismatch",
+		},
+		{
+			name: "error - policy mismatch",
+			modifyReport: func(report *sevsnp.Report) {
+				report.Policy += 1
+			},
+			wantErr: "policy mismatch",
+		},
+		{
+			name: "error - family id mismatch",
+			modifyReport: func(report *sevsnp.Report) {
+				report.FamilyId = []byte("wrong family id")
+			},
+			wantErr: "family id mismatch",
+		},
+		{
+			name: "error - image id mismatch",
+			modifyReport: func(report *sevsnp.Report) {
+				report.ImageId = []byte("wrong image id")
+			},
+			wantErr: "image id mismatch",
+		},
+		{
+			name: "error - vmpl mismatch",
+			modifyReport: func(report *sevsnp.Report) {
+				report.Vmpl += 1
+			},
+			wantErr: "vmpl mismatch",
+		},
+		{
+			name: "error - current tcb mismatch",
+			modifyReport: func(report *sevsnp.Report) {
+				report.CurrentTcb += 1
+			},
+			wantErr: "current tcb mismatch",
+		},
+		{
+			name: "error - platform info mismatch",
+			modifyReport: func(report *sevsnp.Report) {
+				report.PlatformInfo += 1
+			},
+			wantErr: "platform info mismatch",
+		},
+		{
+			name: "error - signer info mismatch",
+			modifyReport: func(report *sevsnp.Report) {
+				report.SignerInfo += 1
+			},
+			wantErr: "signer info mismatch",
+		},
+		{
+			name: "error - measurement mismatch",
+			modifyReport: func(report *sevsnp.Report) {
+				report.Measurement = []byte("wrong measurement")
+			},
+			wantErr: "measurement mismatch",
+		},
+		{
+			name: "error - host data mismatch",
+			modifyReport: func(report *sevsnp.Report) {
+				report.HostData = []byte("wrong host data")
+			},
+			wantErr: "host data mismatch",
+		},
+		{
+			name: "error - id key digest mismatch",
+			modifyReport: func(report *sevsnp.Report) {
+				report.IdKeyDigest = []byte("wrong id key digest")
+			},
+			wantErr: "id key digest mismatch",
+		},
+		{
+			name: "error - author key digest mismatch",
+			modifyReport: func(report *sevsnp.Report) {
+				report.AuthorKeyDigest = []byte("wrong author key digest")
+			},
+			wantErr: "author key digest mismatch",
+		},
+		{
+			name: "error - report id mismatch",
+			modifyReport: func(report *sevsnp.Report) {
+				report.ReportId = []byte("wrong report id")
+			},
+			wantErr: "report id mismatch",
+		},
+		{
+			name: "error - reported tcb mismatch",
+			modifyReport: func(report *sevsnp.Report) {
+				report.ReportedTcb += 1
+			},
+			wantErr: "reported tcb mismatch",
+		},
+		{
+			name: "error - chip id mismatch",
+			modifyReport: func(report *sevsnp.Report) {
+				report.ChipId = []byte("wrong chip id")
+			},
+			wantErr: "chip id mismatch",
+		},
+		{
+			name: "error - committed tcb mismatch",
+			modifyReport: func(report *sevsnp.Report) {
+				report.CommittedTcb += 1
+			},
+			wantErr: "committed tcb mismatch",
+		},
+		{
+			name: "error - current build mismatch",
+			modifyReport: func(report *sevsnp.Report) {
+				report.CurrentBuild += 1
+			},
+			wantErr: "current build mismatch",
+		},
+		{
+			name: "error - current minor mismatch",
+			modifyReport: func(report *sevsnp.Report) {
+				report.CurrentMinor += 1
+			},
+			wantErr: "current minor mismatch",
+		},
+		{
+			name: "error - current major mismatch",
+			modifyReport: func(report *sevsnp.Report) {
+				report.CurrentMajor += 1
+			},
+			wantErr: "current major mismatch",
+		},
+		{
+			name: "error - committed build mismatch",
+			modifyReport: func(report *sevsnp.Report) {
+				report.CommittedBuild += 1
+			},
+			wantErr: "committed build mismatch",
+		},
+		{
+			name: "error - committed minor mismatch",
+			modifyReport: func(report *sevsnp.Report) {
+				report.CommittedMinor += 1
+			},
+			wantErr: "committed minor mismatch",
+		},
+		{
+			name: "error - committed major mismatch",
+			modifyReport: func(report *sevsnp.Report) {
+				report.CommittedMajor += 1
+			},
+			wantErr: "committed major mismatch",
+		},
+		{
+			name: "error - launch tcb mismatch",
+			modifyReport: func(report *sevsnp.Report) {
+				report.LaunchTcb += 1
+			},
+			wantErr: "launch tcb mismatch",
+		},
+		{
+			name: "error - cpuid 1eax fms mismatch",
+			modifyReport: func(report *sevsnp.Report) {
+				report.Cpuid1EaxFms += 1
+			},
+			wantErr: "cpuid 1eax fms mismatch",
+		},
+	}
 
-		// when
-		err := attestation.SEVVerifyMeasurement(measurement, sevReport)
+	for _, tc := range mismatchTestCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// given
+			measurement := sevReportMeasurementJSON
+			timestamp := time.Unix(
+				sevReportTimestampSeconds,
+				sevReportTimestampNanoseconds,
+			)
+			_, sevReport := sevReportFromTestData(t, sevReportB64, timestamp)
+			tc.modifyReport(sevReport)
 
-		// then
-		assert.ErrorContains(t, err, "measurement mismatch: expected")
-	})
+			// when
+			err := attestation.SEVVerifyMeasurement(measurement, sevReport)
+
+			// then
+			assert.ErrorContains(t, err, tc.wantErr)
+		})
+	}
 }
