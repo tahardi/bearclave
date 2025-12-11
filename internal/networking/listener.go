f54594b111bba215c81e0f74c0f4da6d3e9e5015
Taylor Antonio Hardin <Taylor.Antonio.Hardin@gmail.com>
2025-12-11 05:53:42 -0500

    Move parsing code into separate file for readability

--- internal/networking/listener.go
+++ internal/networking/listener.go
@@ -3,8 +3,6 @@ package networking
 import (
 	"fmt"
 	"net"
-	"net/url"
-	"strings"
 
 	"github.com/mdlayher/vsock"
 )
@@ -41,35 +39,4 @@ func NewVSocketListener(_ string, addr string) (net.Listener, error) {
 		return nil, listenerError(msg, err)
 	}
 	return listener, nil
-}
-
-func ParseSocketAddr(addr string) (string, error) {
-	if !strings.Contains(addr, "://") {
-		return addr, nil
-	}
-
-	u, err := url.Parse(addr)
-	if err != nil {
-		return "", fmt.Errorf("%w: parsing URL: %w", ErrSocketParseAddr, err)
-	}
-	return u.Host, nil
-}
-
-func ParseVSocketAddr(addr string) (uint32, uint32, error) {
-	// Handle URLs like "http://cid:port" by stripping the scheme
-	if idx := strings.Index(addr, "://"); idx != -1 {
-		addr = addr[idx+3:]
-	}
-
-	var cid, port int
-	n, err := fmt.Sscanf(addr, "%d:%d", &cid, &port)
-	switch {
-	case err != nil:
-		msg := fmt.Sprintf("expected format 'cid:port' got '%s'", addr)
-		return 0, 0, fmt.Errorf("%w: %s: %w", ErrVSocketParseAddr, msg, err)
-	case n != NumVSockAddrFields:
-		msg := fmt.Sprintf("expected 2 fields got %d", n)
-		return 0, 0, fmt.Errorf("%w: %s", ErrVSocketParseAddr, msg)
-	}
-	return uint32(cid), uint32(port), nil
 }
