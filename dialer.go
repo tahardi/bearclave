f8168489c8cdef11d9e5bcc79114db399428252c
Taylor Antonio Hardin <Taylor.Antonio.Hardin@gmail.com>
2025-12-11 05:44:09 -0500

    Split Dialer and Listener into separate files for readability

--- dialer.go
+++ dialer.go
@@ -2,7 +2,6 @@ package bearclave
 
 import (
 	"fmt"
-	"net"
 
 	"github.com/tahardi/bearclave/internal/networking"
 )
@@ -21,23 +20,6 @@ func NewDialer(platform Platform) (networking.Dialer, error) {
 		return networking.NewSocketDialer()
 	case NoTEE:
 		return networking.NewSocketDialer()
-	default:
-		return nil, fmt.Errorf("%w: %s", ErrUnsupportedPlatform, platform)
-	}
-}
-
-var ErrListener = networking.ErrListener
-
-func NewListener(platform Platform, network string, addr string) (net.Listener, error) {
-	switch platform {
-	case Nitro:
-		return networking.NewVSocketListener(network, addr)
-	case SEV:
-		return networking.NewSocketListener(network, addr)
-	case TDX:
-		return networking.NewSocketListener(network, addr)
-	case NoTEE:
-		return networking.NewSocketListener(network, addr)
 	default:
 		return nil, fmt.Errorf("%w: %s", ErrUnsupportedPlatform, platform)
 	}
